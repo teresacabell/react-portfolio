0dac71cf33d1eb7d22a2f4ade8e107bda4ac9ef2
teresacabell <teresacabll@gmail.com>
2022-04-02 13:51:47 -0500

    added data.js for projects page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
-      <Skills />
       <Contact />
     </main>
   );
---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const projects = [
+    {
+      name: "Run Buddy",
+      liveURL: "https://teresacabell.github.io/run-buddy/",
+      gURL: "https://github.com/teresacabell/run-buddy",
+      tech: "HTML/CSS",
+      style: "runbuddy",
+    },
+    {
+      name: "Flying Dollars",
+      liveUrl: "https://jasonvissage.github.io/Flying-Dollars/",
+      gURL: "https://github.com/jasonvissage/Flying-Dollars",
+      tech: "HTML/CSS/JavaScript/Bulma",
+      style: "flying-dollars",
+    },
+    {
+      name: "Old Fashioned",
+      liveUrl: "https://damp-temple-66157.herokuapp.com/",
+      gUrl: "https://github.com/paul88vera/Old-Fashioned/",
+      tech: "HTML/CSS/JavaScript/Express/mysql2/Sequelize",
+      style: "old-fashioned",
+    },
+  ];
+
+  
