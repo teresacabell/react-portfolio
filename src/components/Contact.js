dcd026b690d37561fef77cae4b2dd414ecd5f4d5
teresacabell <teresacabll@gmail.com>
2022-04-04 11:34:25 -0500

    got contact working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../utils/helpers";
 
 const ContactMe = () => {
   const [formState, setFormState] = useState({
@@ -9,7 +10,7 @@ const ContactMe = () => {
 
   const [errorMessage, setErrorMessage] = useState("");
 
-  const {name, email, message} = formState;
+  const { name, email, message } = formState;
 
   function handleChange(e) {
     if (e.target.name === "email") {
@@ -36,7 +37,33 @@ const ContactMe = () => {
     e.preventDefault();
   }
 
-  return <section className="contact-form" id="contact"></section>;
+  return (
+    <section className="contact" id="contact">
+      <div>
+        <h2>Reach out</h2>
+      </div>
+      <form id="contact-form" onSubmit={handleSubmit}>
+          <div>
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+          </div>
+          <div>
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" defaultValue={email} onBlur={handleChange} />
+          </div>
+          <div>
+              <label htmlFor="message">Message</label>
+              <textarea name="message" rows="7" defaultValue={message} onBlur={handleChange} />
+          </div>
+          {errorMessage && (
+                <div>
+                    <p className="error-text" style={{ color: 'red' }}>{errorMessage}</p>
+                </div>
+            )}
+            <button type="submit">Submit my message</button>
+      </form>
+    </section>
+  );
 };
 
 export default ContactMe;
